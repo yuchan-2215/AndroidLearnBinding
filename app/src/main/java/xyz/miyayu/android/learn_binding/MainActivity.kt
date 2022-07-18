040bb7e0ba8117206be15b10381a27725fc8673c
Miyayu <yuchan2215@gmail.com>
2022-07-18 15:53:47 +0900

    [add] ViewModelをバインディングするようにした

--- app/src/main/java/xyz/miyayu/android/learn_binding/MainActivity.kt
+++ app/src/main/java/xyz/miyayu/android/learn_binding/MainActivity.kt
@@ -2,18 +2,19 @@ package xyz.miyayu.android.learn_binding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import xyz.miyayu.android.learn_binding.databinding.ActivityMainRenamedBinding
+import xyz.miyayu.android.learn_binding.viewmodel.SampleViewModel
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainRenamedBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainRenamedBinding.inflate(layoutInflater)
+        val viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(application).create(SampleViewModel().javaClass)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main_renamed)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
         setContentView(binding.root)
-
-        binding.text.text = getString(R.string.replaced_text)
-        binding.button.setOnClickListener {
-            binding.text.text = getString(R.string.clicked_text)
-        }
     }
 }
