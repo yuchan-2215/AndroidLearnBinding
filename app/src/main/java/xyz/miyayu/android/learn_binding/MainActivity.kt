4c83e250f3a3356ed899d4b65d874acbe90cbd34
Miyayu <yuchan2215@gmail.com>
2022-07-18 15:05:56 +0900

    [change] xmlのファイル名を変更

--- app/src/main/java/xyz/miyayu/android/learn_binding/MainActivity.kt
+++ app/src/main/java/xyz/miyayu/android/learn_binding/MainActivity.kt
@@ -2,13 +2,13 @@ package xyz.miyayu.android.learn_binding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import xyz.miyayu.android.learn_binding.databinding.ActivityMainBinding
+import xyz.miyayu.android.learn_binding.databinding.ActivityMainRenamedBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainRenamedBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainRenamedBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.text.text = getString(R.string.replaced_text)
