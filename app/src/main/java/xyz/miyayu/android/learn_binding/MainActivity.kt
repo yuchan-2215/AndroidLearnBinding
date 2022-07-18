45cf9136ff7f48b3a246941a1e0b22c5a5a3cb2e
Miyayu <yuchan2215@gmail.com>
2022-07-18 14:56:57 +0900

    [add] activity_main.xmlをバインディングした
    [fix] binding

--- app/src/main/java/xyz/miyayu/android/learn_binding/MainActivity.kt
+++ app/src/main/java/xyz/miyayu/android/learn_binding/MainActivity.kt
@@ -2,10 +2,13 @@ package xyz.miyayu.android.learn_binding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import xyz.miyayu.android.learn_binding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
